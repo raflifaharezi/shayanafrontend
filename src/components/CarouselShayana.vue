035653673731b737a445a9c8cabdd7bccf9db558
raflifaharezi <raflifahrezi30@gmail.com>
2020-10-30 15:59:45 +0700

    innitial  commit

---
+++ src/components/CarouselShayana.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="hero-section">
+    <carousel class="hero-items" :items="1" :nav="false" :autoplay="true">
+      <div
+        class="single-hero-items set-bg"
+        style="background-image: url('/img/hero-1.jpg')"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1>Indenpenden Day</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore
+              </p>
+              <a href="#" class="primary-btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="single-hero-items set-bg"
+        style="background-image: url('/img/hero-2.jpg')"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1>Discount Up To 30%</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore
+              </p>
+              <a href="#" class="primary-btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="single-hero-items set-bg"
+        style="background-image: url('/img/pexels6.jpg')"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <span>Bag,kids</span>
+              <h1 class="text-white">Nike Air</h1>
+              <p class="text-white">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore
+              </p>
+              <a href="#" class="primary-btn">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </carousel>
+  </section>
+</template>
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "CarouselShayana",
+  components: {
+    carousel,
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayana />
+    <CarouselShayana />
+    <BannerShayana />
+    <InstaShayna />
+    <PartnerShayana />
+    <footerShayana />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayana from "@/components/HeaderShayana.vue";
+import CarouselShayana from "@/components/CarouselShayana.vue";
+import BannerShayana from "@/components/BannerShayana.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayana from "@/components/PartnerShayana.vue";
+import FooterShayana from "@/components/FooterShayana.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayana,
+    CarouselShayana,
+    BannerShayana,
+    InstaShayna,
+    PartnerShayana,
+    FooterShayana,
+  },
+};
+</script>
